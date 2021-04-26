2b407dea7b6d04ba635bccdb30d8fc78a05110a2
gcolossi <colossig@gmail.com>
2021-04-26 15:20:32 -0300

    projeto full

---
+++ src/pages/_app.tsx
@@ -0,0 +1,23 @@
+import "../styles/global.scss";
+
+import React, { useContext, useState } from "react";
+import { Header } from "../components/Header";
+import styles from "../styles/app.module.scss";
+import { Player } from "../components/Player";
+import { PlayerContext, PlayerContextProvider } from '../contexts/PlayerContext'
+
+function MyApp({ Component, pageProps }) {
+return (
+  <PlayerContextProvider>
+    <div className={styles.wrapper}>
+      <main>
+        <Header />
+        <Component {...pageProps} />
+      </main>
+      <Player />
+    </div>
+    </PlayerContextProvider>
+  );
+}
+
+export default MyApp;
